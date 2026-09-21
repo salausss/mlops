54da73ef7067950c3c41da24a4e0d40b1ecea7b9
salausss <salauss00@gmail.com>
2026-09-22 01:31:05 +0530

    sdf

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -12,7 +12,7 @@ module "vpc" {
  
 module "s3_bucket" {
   source      = "../../modules/s3"
-  bucket_name = "mlops-dev-uploads-002"
+  bucket_name = "mlops-dev-uploads-004"
 
   versioning_enabled   = true
   enable_encryption    = true
